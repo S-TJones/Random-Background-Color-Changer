d9aad955f20c44bf878345462166eb4bf26d7f03
S-TJones <jones.shemar.sj@gmail.com>
2020-08-06 19:46:58 -0500

    Added some JS to randomly select colors from an array of colors.

--- Color Switcher/Scripts/script.js
+++ Color Switcher/Scripts/script.js
@@ -1,5 +1,21 @@
 
 const colors = ["green", "red", "rgba(133, 122, 200)", "#f15025"];
 
-const btn = document.getElementById("btn");
-const color = document.querySelector();
+const button = document.getElementById("change-btn");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", function() {
+
+    // Gets a random number between 0 and 2.
+    // NOTE: Incldes decimals.
+    // const randomNum = 2;
+    const randomNum = getRandomNumber();
+    // console.log(randomNum);
+
+    document.body.style.backgroundColor = colors[randomNum];
+    color.textContent = colors[randomNum];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
